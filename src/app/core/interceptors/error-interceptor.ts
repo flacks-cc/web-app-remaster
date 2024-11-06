6c5ac9b75c26a2cdcc5de2fa001b536b113314f6
ChristianSp <christiansp7231@gmail.com>
2024-11-06 14:16:02 -0600

    Eliminación de manejo de errores en el interceptor y ajuste en el componente de login

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -3,8 +3,8 @@ import { catchError, EMPTY } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
-    catchError(() => {
-      return EMPTY;
-    })
+    // catchError(() => {
+    //   return EMPTY;
+    // })
   );
 };
